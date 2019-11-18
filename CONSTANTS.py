f1412bcb07de175972002540ab4c0653ecd83a48
MasterDoo <masterdu9951@icloud.com>
2019-11-18 01:46:42 -0500

    add dir

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -2,6 +2,7 @@
 # Directories
 DIR_PROJECT = "/Users/tianyudu/Documents/UToronto/Course/ECO499/ugthesis/"
 DIR_ECONOMIC_CALENDAR_DATA = DIR_PROJECT + "data/economic_calendar/"
+DIR_STOCK_DATA = DIR_PROJECT + "data/stock/"
 
 # API Locations
 QUANDL_API = "/Users/tianyudu/Documents/UToronto/Course/ECO499/quandl_api.txt"
