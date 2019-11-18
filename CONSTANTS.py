cc71ab4df4f34514cdfd326e54ed3a7e4abc6c7d
MasterDoo <masterdu9951@icloud.com>
2019-11-17 23:06:02 -0500

    add new directories

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -2,3 +2,7 @@
 # Directories
 DIR_PROJECT = "/Users/tianyudu/Documents/UToronto/Course/ECO499/ugthesis/"
 DIR_ECONOMIC_CALENDAR_DATA = DIR_PROJECT + "data/economic_calendar/"
+
+# API Locations
+QUANDL_API = "/Users/tianyudu/Documents/UToronto/Course/ECO499/quandl_api.txt"
+STOCK_NEWS_API = "/Users/tianyudu/Documents/UToronto/Course/ECO499/stock_news_api.txt"
