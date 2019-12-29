d38fd6f28e0006709e17b65d64100182e1fde1b7
MasterDoo <masterdu9951@icloud.com>
2019-12-29 12:45:18 +0800

    update main

--- tsclustering/tsclust.py
+++ tsclustering/tsclust.py
@@ -1,9 +1,73 @@
 """
 Time series clustering.
 """
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-from tsclust_utils import *
+
+import tsclust_utils
+
+
+def main(
+    df_wti_real: pd.DataFrame,
+    df_wti_return: pd.DataFrame,
+    verbose: bool = True
+) -> None:
+    if verbose:
+        print("REAL PRICE series received:")
+        print(df_wti_real.info())
+        print("LOG-DIFF RETURN ON REAL PRICE series received:")
+        print(df_wti_return.info())
+    mo_subseqs, mo_stats = tsclust_utils.monthly_subsequence(df_wti_return)
+    df_gmm_labels = tsclust_utils.time_series_clustering(
+        mo_stats,
+        n_clusters=3,
+        normalize=True
+    )
+    df_wti_return_labelled, df_wti_real_labelled = map(
+        lambda x: tsclust_utils.broadcast_monthly_clustering(
+            x, df_gmm_labels
+        ),
+        (df_wti_return, df_wti_real)
+    )
+
+    tsclust_utils.regime_plot(
+        df_wti_real_labelled,
+        df_wti_real_labelled["label"].values,
+        "DCOILWTICO_REAL",
+        save_dir="./figures/wti_real_regime.png"
+    )
+
+    tsclust_utils.regime_plot(
+        df_wti_return_labelled,
+        df_wti_return_labelled["label"].values,
+        "DCOILWTICO_RETURN_REAL",
+        save_dir="./figures/wti_return_regime.png"
+    )
+
+    
+
 
 if __name__ == "__main__":
-    df_wti_real.dropna().to_csv("../data/ready_to_use/wti_crude_oil_price_real.csv")
-    df_wti_return.dropna().to_csv("../data/ready_to_use/wti_crude_oil_return_real.csv")
+    # Matplotlib Conifg
+    plt.rcParams["figure.figsize"] = (15, 3)
+    plt.rcParams["figure.dpi"] = 300
+    plt.rcParams["axes.grid"] = True
+    # Load data
+    df_wti_real = pd.read_csv(
+        "./data/ready_to_use/wti_crude_oil_price_real.csv",
+        index_col=0,
+        header=0,
+        parse_dates=["DATE"],
+        date_parser=lambda d: datetime.strptime(d, "%Y-%m-%d")
+    )
+    df_wti_return = pd.read_csv(
+        "./data/ready_to_use/wti_crude_oil_return_real.csv",
+        index_col=0,
+        header=0,
+        parse_dates=["DATE"],
+        date_parser=lambda d: datetime.strptime(d, "%Y-%m-%d")
+    )
+    main(df_wti_real, df_wti_return)
