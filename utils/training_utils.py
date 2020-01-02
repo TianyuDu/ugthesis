00b3bb6beced106e909b11a8ff0d07a219094328
MasterDoo <masterdu9951@icloud.com>
2020-01-02 20:05:31 +0800

    batch_sampler

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -1,3 +1,26 @@
 """
 Utilities for training neural networks.
 """
+from typing import List, Tuple
+
+
+def batch_sampler(
+    batch_size: int,
+    data_size: int
+) -> List[Tuple[int]]:
+    """
+    Generates a sequence of (low, high) index pairs for mini-batches.
+    Usage:
+        for (low, high) in batch_sampler:
+            mini_batch = X[low: high, :]
+    """
+    if batch_size >= data_size:
+        return (0, data_size)  # returns the entire dataset.
+    lst = list()
+    low = 0
+    for i in range(data_size // batch_size):
+        high = low + batch_size
+        lst.append((low, high))
+        low = high
+    lst.append((low, data_size))
+    return lst
