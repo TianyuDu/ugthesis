cd8983e1569c91132e4dd6bf8592690b436feb7a
Tianyu Du <tianyudu@Tianyus-Manifold.local>
2020-01-20 17:01:06 -0500

    update eco421

--- eda/master_data_proc.py
+++ eda/master_data_proc.py
@@ -1,3 +1,22 @@
 """
 Aggregate data processing utility, generate the ready to use dataset.
 """
+import argparse
+
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+
+def main(
+    src: str
+) -> None:
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    # Add configuration.
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+    raise NotImplementedError
