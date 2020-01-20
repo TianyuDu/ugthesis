1067b56b2a2156c2e27f36086ce3ab2f04a2a585
Tianyu Du <tianyudu@Tianyus-Manifold.local>
2020-01-20 17:33:17 -0500

    update

--- eda/master_data_processing.py
+++ eda/master_data_processing.py
@@ -2,15 +2,25 @@
 Aggregate data processing utility, generate the ready to use dataset.
 """
 import argparse
-
+import json
+import os
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
 
 
+def rpna(config: dict):
+    raise NotImplementedError
+
+
+def crude_oil(config: dict):
+    raise NotImplementedError
+
+
 def main(
-    src: str
+    src: str,
+    config: dict
 ) -> None:
     raise NotImplementedError
 
@@ -18,5 +28,11 @@ def main(
 if __name__ == "__main__":
     # Add configuration.
     parser = argparse.ArgumentParser()
-    parser.add_argument("--save_to", default="./aggregate_")
-    raise NotImplementedError
+    parser.add_argument("--save_to", default="./master_dataset.csv", type=str)
+    parser.add_argument("--config", type=str)
+    args = parser.parse_args()
+    assert os.path.exists(args.config)
+    print(f"Read configuration from {args.config}")
+    with open(args.config, "r") as f:
+        config = json.load(f)
+        print(config)
