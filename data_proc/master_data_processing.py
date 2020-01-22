482c75961df64725dfba5b93c413c505801e6588
tianyudu <masterdu9951@icloud.com>
2020-01-22 12:41:08 -0500

    update

--- data_proc/master_data_processing.py
+++ data_proc/master_data_processing.py
@@ -5,10 +5,12 @@ import argparse
 import json
 import os
 from datetime import datetime
+from typing import List, Union
 
 import numpy as np
 import pandas as pd
 
+import fred_macro_features
 import rpna_processing
 
 
@@ -66,34 +68,57 @@ def _generate_lags(
     for L in lags:
         df_lagged = df.shift(L)
         df_lagged.columns = [
-            x + f"_{L}"
+            x + f"_L{L}"
             for x in cols
         ]
         collection.append(df_lagged)
     # TODO: reorder the columns.
-    return pd.concat(collection, aixs=1)
+    return pd.concat(collection, axis=1)
+
+# df2 = _generate_lags(df, 3)
 
 
-
-def _check_df_equal(df1, df2) -> bool:
+def _check_df_equal(df1: pd.DataFrame, df2: pd.DataFrame) -> bool:
     out = list()
     for col in df1.columns:
         out.append(np.sum(df1[col] - df2[col]) < 1e-12)
     for col in df2.columns:
         out.append(np.sum(df1[col] - df2[col]) < 1e-12)
-    return all(out)
+    assert set(df1.columns) == set(df2.columns), "Columns not matched."
+    assert all(out), "Value difference."
 
 
 def main(
     config: dict
 ) -> None:
     df_lst = list()
+    # Load crude oil price.
+    df_oil_price = _load_wti(
+        src_file=config["oil.src"]
+    )
+    df_lst.append(df_oil_price)
 
+    # Load RPNA dataset on crude oil.
     df_rpna_oil = _load_rpna(
         src_file=config["rpna.crude_oil.src"],
         radius=config["rpna.radius"]
     )
     df_lst.append(df_rpna_oil)
+
+    # Load macro variables from Fred.
+    df_macro = _load_macro(
+        src_file=config["fred.src"]
+    )
+    df_lst.append(df_macro)
+
+    # Convert to business days.
+    df_lst = [d.asfreq("B") for d in df_lst]
+
+    # Add lags
+    # NOTE: macro variables are already lagged variables
+    # which indicate measures in the previous measuring period (e.g., month).
+    df_lst = [_generate_lags(d) for d in df_lst]
+    df_lst.append(df_macro)
 
 
 # Testing utilities
