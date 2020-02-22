354935e8afcc196412fd9e7bc0795f140e6ff961
tianyudu <masterdu9951@icloud.com>
2020-02-22 13:55:26 -0500

    add

---
+++ models/data_feed.py
@@ -0,0 +1,61 @@
+"""
+Date: Feb. 22, 2020.
+Data loaders for most models.
+"""
+import sys
+sys.path.append("../")
+
+import numpy as np
+import pandas as pd
+
+from typing import List, Tuple
+from datetime import datetime
+
+from sklearn import model_selection
+
+from utils.time_series_utils import gen_dataset_calendarday
+
+# ============ Configs ============
+MASTER_DIR = "/Users/tianyudu/Documents/UToronto/Course/ECO499/ugthesis"
+TARGET_COL = "RETURN"
+LAG_DAYS = 28
+DF = pd.read_csv(
+    MASTER_DIR + "/data/ready_to_use/returns_norm.csv",
+    date_parser=lambda x: datetime.strptime(x, "%Y-%m-%d"),
+    index_col=0
+)
+# ============== End ==============
+
+
+def all_valid_verification(X: pd.DataFrame, y: pd.DataFrame) -> bool:
+    """
+    A verification method requires all entries in both feature set and label set to be non-null.
+    This function depends on specific case.
+    """
+    if np.any(X.isnull()):
+        return False
+    elif np.any(y.isnull()):
+        return False
+    elif len(X) == 0:
+        return False
+    return True
+
+
+def fix_failed(X: pd.DataFrame, y: pd.DataFrame) -> Tuple[pd.DataFrame]:
+    pass
+
+
+def regression_feed() -> List[np.ndarray]:
+    """
+    Feed training and testing sets to the model evaluation method.
+    Note that validation set will be extracted from the training set.
+    """
+    feature_list, label_list, failed_feature_list, failed_label_list = gen_dataset_calendarday(
+        DF,
+        TARGET_COL,
+        LAG_DAYS,
+        verify=all_valid_verification
+    )
+    ds_passed = list(zip(feature_list, label_list))
+    ds_failed = list(zip(failed_feature_list, failed_label_list))
+    
