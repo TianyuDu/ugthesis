5af4e456c6c06238d92423d185bf4bccf5c16a73
tianyudu <masterdu9951@icloud.com>
2020-02-27 17:35:00 -0500

    update

--- models/training.py
+++ models/training.py
@@ -1,3 +1,38 @@
 """
 Training and model evaluation procedures.
 """
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+from typing import List, Tuple
+
+from sklearn.model_selection import train_test_split
+
+
+def check_data(data) -> None:
+    """
+    Validate dataset input.
+    """
+    raise NotImplementedError
+
+def train(
+    model,
+    data: List[np.ndarray]
+) -> dict():
+    """
+    Train and evaluate model.
+    """
+    # ==== Data Preprocessing ====
+    X_train, y_train, X_test, y_test = data
+    # Train validation split.
+    # Note that the order of dataset returned by
+    # train_test_split method is odd.
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train,
+        train_size=0.75,
+        shuffle=True
+    )
+    # ==== Training procedure ====
+    raise NotImplementedError
