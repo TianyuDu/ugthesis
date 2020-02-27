94f15f0eb13faa9133b3d6c49b7c235bb18f4ee5
tianyudu <masterdu9951@icloud.com>
2020-02-27 17:49:20 -0500

    update

--- models/training.py
+++ models/training.py
@@ -2,13 +2,14 @@
 Training and model evaluation procedures.
 """
 from datetime import datetime
+from typing import List, Tuple, Union
 
 import numpy as np
 import pandas as pd
-
-from typing import List, Tuple
-
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+
+from data_feed import regression_feed
 
 
 def check_data(data) -> None:
@@ -17,22 +18,28 @@ def check_data(data) -> None:
     """
     raise NotImplementedError
 
-def train(
+
+def training_pipeline(
     model,
-    data: List[np.ndarray]
+    data: List[np.ndarray],
+    task: Union["regression", "classification"],
+    num_fold: int = 1,
 ) -> dict():
     """
     Train and evaluate model.
     """
     # ==== Data Preprocessing ====
-    X_train, y_train, X_test, y_test = data
-    # Train validation split.
-    # Note that the order of dataset returned by
-    # train_test_split method is odd.
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train, y_train,
-        train_size=0.75,
-        shuffle=True
-    )
-    # ==== Training procedure ====
-    raise NotImplementedError
+    X_train_raw, y_train_raw, X_test, y_test = data
+    check_data(data)
+    val_loss_lst = list()
+    for _ in range(num_fold):
+        # Train validation split.
+        # Note that the order of dataset returned by
+        # train_test_split method is odd.
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train_raw, y_train_raw,
+            train_size=0.75,
+            shuffle=True
+        )
+        model.fit(X_train, y_train)
+        pred_val = model.predict(X_val)
