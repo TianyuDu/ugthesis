30d99de3fd1ee6079bf3fac743e3677cd7e27f94
tianyudu <masterdu9951@icloud.com>
2020-04-09 20:36:30 -0400

    add new metrics

--- models/training_utils.py
+++ models/training_utils.py
@@ -5,4 +5,18 @@ import numpy as np
 
 
 def directional_accuracy(y_true, y_pred):
+    """
+    The directionary accuracy metric.
+    Identity {sign(target) == sign(prediction)}
+    """
     return float(np.mean(np.sign(y_true) == np.sign(y_pred)))
+
+
+def mape(y_true, y_pred):
+    """
+    Mean absolute percentage error.
+    """
+    base = np.abs(y_true)
+    base[base == 0.0] = np.inf  # To avoid infinite.
+    err = np.abs(y_pred - y_true)
+    return np.mean(err / base) * 100
