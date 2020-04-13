2b24765e65bad063856b1226985f540cdea6f8dd
tianyudu <masterdu9951@icloud.com>
2020-04-13 14:19:34 -0400

    add new script

---
+++ models/model_testing.py
@@ -0,0 +1,23 @@
+"""
+Evaluate model performance on the test set.
+"""
+import numpy as np
+import pandas as pd
+
+
+def test_rf():
+    pass
+
+
+def test_svr():
+    pass
+
+
+def test_lstm():
+    pass
+
+
+if __name__ == "__main__":
+    test_rf()
+    test_svr()
+    test_lstm()
