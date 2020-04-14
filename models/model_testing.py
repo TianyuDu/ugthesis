bff37810c8ed6b3ab13ad49ff47bedf492e45098
tianyudu <masterdu9951@icloud.com>
2020-04-13 23:04:25 -0400

    update

--- models/model_testing.py
+++ models/model_testing.py
@@ -6,11 +6,22 @@ import pandas as pd
 
 from pprint import pprint
 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+
 from data_feed import direct_feed
 
 
-def test_rf(X_test: np.ndarray, y_test: np.ndarray, config: dict):
-    pass
+def test_rf(
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    y_train: np.ndarray,
+    y_test: np.ndarray,
+    config: dict
+) -> None:
+    model = RandomForestRegressor(**config)
+    model.fit(X_train)
+    model.predict(X_test)
 
 
 def test_svr(X_test: np.ndarray, y_test: np.ndarray, config: dict):
@@ -18,8 +29,27 @@ def test_svr(X_test: np.ndarray, y_test: np.ndarray, config: dict):
 
 
 def main():
+    svr_alldays_mse = {}
+    svr_alldays_da = {}
+    rf_allday_mse = {}
+    rf_allday_da = {}
+
+    svr_monday_mse = {}
+    svr_monday_da = {}
+    rf_monday_mse = {}
+    rf_monday_da = {}
+
+    svr_otherdays_mse = {}
+    svr_otherdays_da = {}
+    rf_otherdays_mse = {}
+    rf_otherdays_da = {}
+
     day_lst = [["Monday"], ["Tuesday", "Wednesday", "Thursday", "Friday"], None]
-    config_lst = [(), (), ()]
+    config_lst = [
+        (rf_monday, svr_monday),
+        (rf_otherdays, svr_otherdays),
+        (rf_alldays, svr_alldays),
+    ]
     for day, (rf_config, svr_config) in zip(day_lst, config_lst):
         print(f"================= {day} =================")
         X_train, X_test, y_train, y_test = direct_feed(
@@ -35,11 +65,11 @@ def main():
 
         print("==== Random Forest ====")
         pprint(rf_config)
-        test_rf(X_test, y_test, rf_config)
+        test_rf(X_train, X_test, y_train, y_test, rf_config)
 
         print("==== Support Vector Regressions ====")
         pprint(svr_config)
-        test_svr(X_test, y_test, svr_config)
+        test_svr(X_train, X_test, y_train, y_test, svr_config)
 
 
 if __name__ == "__main__":
