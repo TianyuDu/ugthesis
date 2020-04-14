d45827a55b5f9d339bf9412479fee2beab55e4fd
tianyudu <masterdu9951@icloud.com>
2020-04-13 23:13:44 -0400

    record optimal hyper-parameters

--- models/model_testing.py
+++ models/model_testing.py
@@ -29,20 +29,32 @@ def test_svr(X_test: np.ndarray, y_test: np.ndarray, config: dict):
 
 
 def main():
-    svr_alldays_mse = {}
-    svr_alldays_da = {}
-    rf_allday_mse = {}
-    rf_allday_da = {}
+    svr_alldays_mse = {"tol": 1, "kernel": "rbf", "gamma": 1e-06, "epsilon": 0.001, "C": 1e-09}
+    svr_alldays_da = {"tol": 1e-05, "kernel": "rbf", "gamma": 1e-07, "epsilon": 1e-07, "C": 10}
 
-    svr_monday_mse = {}
-    svr_monday_da = {}
-    rf_monday_mse = {}
-    rf_monday_da = {}
+    rf_allday_mse = {"n_estimators": 96, "min_samples_split": 10,
+                     "min_samples_leaf": 2, "max_features": "log2", "max_depth": 10, "bootstrap": True}
 
-    svr_otherdays_mse = {}
-    svr_otherdays_da = {}
-    rf_otherdays_mse = {}
-    rf_otherdays_da = {}
+    rf_allday_da = {"n_estimators": 41, "min_samples_split": 5, "min_samples_leaf": 4,
+                    "max_features": None, "max_depth": 14, "bootstrap": False}
+
+    svr_monday_mse = {"tol": 0.001, "kernel": "rbf", "gamma": 1e-10, "epsilon": 0.0001, "C": 10}
+    svr_monday_da = {"tol": 0.1, "kernel": "rbf", "gamma": 1e-06, "epsilon": 1e-06, "C": 1}
+
+    rf_monday_mse = {"n_estimators": 115, "min_samples_split": 10,
+                     "min_samples_leaf": 4, "max_features": "log2", "max_depth": 38, "bootstrap": True}
+
+    rf_monday_da = {"n_estimators": 116, "min_samples_split": 2, "min_samples_leaf": 1,
+                    "max_features": "log2", "max_depth": 38, "bootstrap": False}
+
+    svr_otherdays_mse = {"tol": 0.1, "kernel": "rbf", "gamma": 0.1, "epsilon": 0.0001, "C": 10}
+    svr_otherdays_da = {"tol": 0.1, "kernel": "rbf", "gamma": 1e-09, "epsilon": 1e-07, "C": 1}
+
+    rf_otherdays_mse = {"n_estimators": 88, "min_samples_split": 5,
+                        "min_samples_leaf": 4, "max_features": "log2", "max_depth": 10, "bootstrap": True}
+
+    rf_otherdays_da = {"n_estimators": 157, "min_samples_split": 2,
+                       "min_samples_leaf": 1, "max_features": None, "max_depth": 10, "bootstrap": False}
 
     day_lst = [["Monday"], ["Tuesday", "Wednesday", "Thursday", "Friday"], None]
     config_lst = [
